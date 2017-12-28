cb12212ed6086115b7c3c43e67822d10d18957b5
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-28 15:22:40 +0530

    Update and rename TestClient.java to TestClient_Save.java

--- com/pratik/test/TestClient_Save.java
+++ com/pratik/test/TestClient_Save.java
@@ -7,7 +7,7 @@ import org.hibernate.Transaction;
 import com.pratik.entity.EmpDetails;
 import com.pratik.utility.HibernateUtil;
 
-public class TestClient {
+public class TestClient_Save {
 
 	public static void main(String[] args) {
 		Session ses=null;
