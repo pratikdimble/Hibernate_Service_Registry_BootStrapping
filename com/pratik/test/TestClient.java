f9e31c70498c0b36067912a678bfa251acaebb7a
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-28 15:21:56 +0530

    Create TestClient.java

---
+++ com/pratik/test/TestClient.java
@@ -0,0 +1,56 @@
+package com.pratik.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.pratik.entity.EmpDetails;
+import com.pratik.utility.HibernateUtil;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		Session ses=null;
+		boolean flag=false;
+		Transaction tx=null;
+		EmpDetails details=null;
+		int idVal=0;
+		//get Session 
+		ses=HibernateUtil.getSession();
+		
+		try{
+		  tx=ses.beginTransaction();
+		  //create domain class obj
+		    details=new EmpDetails();
+		    details.setNo(1008);
+		    details.setFname("Pratik");
+		    details.setLname("Dimble");
+		    details.setMail("pd@gmail.com");
+		    //save object
+		    idVal=(int)ses.save(details);
+		    System.out.println("Generated id value::"+idVal);
+		  flag=true;
+		}
+		catch(HibernateException he){
+			he.printStackTrace();
+			flag=false;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			flag=false;
+		}
+		finally{
+			if(flag==true){
+				tx.commit();
+				System.out.println("Object saved");
+			}
+			else{
+				tx.rollback();
+				System.out.println("Object not saved");
+			}
+			
+			HibernateUtil.closeSession(ses);
+			HibernateUtil.closeSessionFactory();
+		}//finally
+	}//main
+}//class
